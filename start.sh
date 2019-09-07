d76f6d2d6a2411872beb657bb37b87575fd80388
Jack Spargo <jack.spargo@gmail.com>
2019-09-07 18:54:09 +0100

    switch localhost to 127.0.0.1

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 rm -rf static
 npm install -g ghost-static-site-generator
 npm start > /dev/null & sleep 5 && \
-gssg --domain 'http://localhost:2368' --url 'https://www.letssewbelfast.com' --dest static
+gssg --domain 'http://127.0.0.1:2368' --url 'https://www.letssewbelfast.com' --dest static
 lsof -ti tcp:2368 | xargs kill
