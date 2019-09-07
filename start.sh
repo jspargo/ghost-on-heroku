7e31d4ca7ff1ad9de0bd71b8f94de806f97347fe
Jack Spargo <jack.spargo@gmail.com>
2019-09-07 18:51:36 +0100

    cleanup actions

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf static
 npm install -g ghost-static-site-generator
 npm start > /dev/null & sleep 5 && \
 gssg --domain 'http://localhost:2368' --url 'https://www.letssewbelfast.com' --dest static
+lsof -ti tcp:2368 | xargs kill
