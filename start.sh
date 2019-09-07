7454ad87a703b3e8c75e726ada36f77d46fcb1af
Jack Spargo <jack.spargo@gmail.com>
2019-09-07 16:31:06 +0100

    attempting to use "ghost-static-site-generator"

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# npm install -g ghost-static-site-generator
+npm start > /dev/null & sleep 5 && gssg --domain 'http://localhost:2368' --url 'https://www.letssewbelfast.com' --dest static
